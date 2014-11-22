a56942ae6c98ec3d0100b85386b198e969f52ab4
Dr.B <drbaguiar@gmail.com>
2014-11-22 00:09:06 -0600

    bgb

---
+++ Bootstrap2.R
@@ -0,0 +1,15 @@
+rm(list=ls())
+library(UsingR)
+data(father.son)
+x <- father.son$sheight
+n <-length(x)
+b <- 10000 ##Bootstrap number of samples 
+resamples <- matrix(sample(x,n*b,replace=TRUE),b,n)
+medians <-apply (resamples,1,median)
+hist(medians,main = paste("Histogram of \n", b, "Bootstraps" ))
+rug(medians)
+abline(v=median(x),col="red")
+sd(medians)
+quantile(medians, c(.025,.975))
+abline(v=quantile(medians, c(.025)),col="blue")
+abline(v=quantile(medians, c(.975)),col="blue")
