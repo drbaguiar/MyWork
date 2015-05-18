96e5a74c66bc307911269bce5f37c0604d630223
Bryan Aguiar <drbaguiar@gmail.com>
2015-05-17 20:49:31 -0500

    Updated

--- GenericLoadnCLean.R
+++ GenericLoadnCLean.R
@@ -14,14 +14,15 @@ set.seed(2345)
 library(psych)
 library(e1071)
 library(caret)
+library(fBasics)
 
 # Load the data
-#df<-read.csv(file.choose())
-df <-read.csv("d:/data/diabetes.csv")
+df<-read.csv(file.choose())
+#df <-read.csv("d:/data/diabetes.csv")
 
 # count blanks remove blanks
 colSums(!is.na(df))
-#df <- na.omit(df)
+df <- na.omit(df)
 #colSums(!is.na(df))
 
 # clean the data names and data
@@ -34,19 +35,19 @@ names(df) <- gsub("-","",names(df))
 names(df) <- gsub(",","",names(df))
 
 # remove a column
-#df$obsno <-NULL
+df$policynumber <-NULL
   
 # REQUIRES caret package to split
 # separate data into test and train sets, 75/25 split in this case
-splitIndex <- createDataPartition(df$diabetes, p = 0.75, list = FALSE)
+splitIndex <- createDataPartition(df$survived, p = 0.75, list = FALSE)
 train <- df[splitIndex, ]
 test <- df[-splitIndex, ]
 
 # Make some data frames to use
-testInd <- test[ ,!colnames(test) %in% "diabetes"]
-testDep <- as.factor(test[, names(test) == "diabetes"]) 
-trainInd <- train[ ,!colnames(train) %in% "diabetes"]
-trainDep <- as.factor(train[, names(train) == "diabetes"]) 
+testInd <- test[ ,!colnames(test) %in% "survived"]
+testDep <- as.factor(test[, names(test) == "survived"]) 
+trainInd <- train[ ,!colnames(train) %in% "survived"]
+trainDep <- as.factor(train[, names(train) == "survived"]) 
 
 # do the random split (25% held out for test), put the label back into the data frame
 df$istest <- runif(nrow(df))<0.25
@@ -62,6 +63,10 @@ dftest<-dftest[,!names(dftest) %in% cols]
 #remove uneeded variables and dataframes
 rm(df,splitIndex,test,train,cols)
 
+# Add log 
+dftrain$loglosses<-log(dftrain$losses)
+dftest$loglosses<-log(dftest$losses)
+
 # attach for working
 attach(dftrain)
 
@@ -74,14 +79,27 @@ sapply(dftrain,mean)
 sapply(dftrain,median)
 sapply(dftrain,sd)
 
+# REQUIRES fBasics package
+basicStats(dftrain) #Computed basic stats
+colStats(dftrain)
+colMeans(dftrain) # Computes sample mean by col
+colVars(dftrain) # Computes sample variance  by col
+colSds(dftrain) # Computes sample standard deviation  by col
+colSkewness (dftrain$age) # Computes sample skewness by col
+colKurtosis (dftrain) # Computes sample kurtosis by col
+colMaxs (dftrain) # Computes maximum values in each col
+colMins (dftrain) # Computes minimum values in each col
+colProds (dftrain) # Computes product of values in each col
+colQuantiles(dftrain, prob=.10)#Computes product of values in each col
+
 # Continous data use type=6
-quantile(plasmaglucose, probs = c(25, 50, 75)/100, type=6)
+quantile(plasmaglucose, probs = c(5,10,25,50,75)/100, type=6)
 IQR(plasmaglucose,type=6)
 fivenum(plasmaglucose)
 
 # REQUIRES psych package
-describe(plasmaglucose, type=1)
-describeBy(plasmaglucose,diabetes, type=1)
+describe(dftrain$age, type=1)
+describeBy(dftrain$age,dftrain$married, type=1)
 
 #aggreate
 aggregate(plasmaglucose~diabetes,mean,data=dftrain)
@@ -99,28 +117,25 @@ skewness(plasmaglucose,type=1)
 kurtosis(plasmaglucose, type=1)
 
 # View some basic boxplots
-boxplot(plasmaglucose~diabetes, xlab="Diabetes", ylab="Plasma Glucose")
-boxplot(bmi~diabetes, xlab="Diabetes", ylab="Body Mass Index")
+boxplot(dftrain$age~dftrain$survived, xlab="Survived", ylab="Age")
+boxplot(dftrain$logfare~dftrain$survived, xlab="Survived", ylab="Log Fare")
 boxplot(diastolic~diabetes, xlab="Diabetes", ylab="Diastolic Blood Pressure")
 boxplot(x2hourseruminsulin~diabetes, xlab="Diabetes", ylab="2 Hour Serum Insulin")
 
 # View a dotplot
-dotchart(plasmaglucose, xlab="Plasma Glucose")
+dotchart(dftrain$logfare, xlab="Plasma Glucose")
 
 # View Stripchart
-stripchart(plasmaglucose~diabetes,method="jitter", pch=c(1,2), col=c("red","blue"), xlab="Plasma Glucose", ylab="Diabetes", main="Diabetes vs. Plasma Glucose",offset=0.5)
-stripchart(plasmaglucose~diabetes,method="stack", pch=c(1,2), col=c("red","blue"), xlab="Plasma Glucose", ylab="Diabetes", main="Diabetes vs. Plasma Glucose",offset=0.5)
+stripchart(dftrain$age~dftrain$survived,method="jitter", pch=c(1,2), col=c("red","blue"), xlab="Age", ylab="Survived", main="Age vs. Survived",offset=0.5)
+stripchart(dftrain$age~dftrain$survived,method="stack", pch=c(1,2), col=c("red","blue"), xlab="Plasma Glucose", ylab="Diabetes", main="Diabetes vs. Plasma Glucose",offset=0.5)
 
 # two-way contingency table of categorical outcome and predictors we want
 #  to make sure there are not 0 cells
-xtabs(~diabetes + timespregnant, data = dftrain)
+xtabs(~survived + age, data = dftrain)
+summary(xtabs(~survived + age, data = dftrain))
 
-
-
-
-
-
-
+xtabs(~survived + logfare, data = dftrain)
+summary(xtabs(~survived + logfare, data = dftrain))
 
 
 
@@ -150,7 +165,7 @@ summary(extracted)
 aggregate(dftrain, by=list(hcluster),FUN=mean, na.rm=TRUE)
 
 # Compare
-column <- timespregnant
+column <- married
 with(dftrain, table(hcluster, column))
 barplot(with(dftrain, table(hcluster, column)),col=c("red","green","blue"),beside = TRUE)
 
@@ -163,7 +178,7 @@ harmonic.mean(dftrain$age,na.rm=TRUE)
 
 ##Classification Tree
 library(rpart)
-fit <- rpart(diabetes ~ ., method="class", data=dftrain)
+fit <- rpart(hcluster ~ ., method="class", data=dftrain)
 
 printcp(fit) # display the results 
 plotcp(fit) # visualize cross-validation results 
@@ -187,7 +202,7 @@ post(pfit, file = "c:/ptree.ps", title = "Pruned Classification Tree")
 library(rpart)
 
 # grow tree 
-fit <- rpart(diabetes ~ .,method="anova", data=dftrain)
+fit <- rpart(hcluster ~ .,method="anova", data=dftrain)
 
 printcp(fit) # display the results 
 plotcp(fit) # visualize cross-validation results 
@@ -213,7 +228,7 @@ post(pfit, file = "c:/ptree2.ps",title = "Pruned Regression Tree")
 
 # Random Forest prediction 
 library(randomForest)
-fit <- randomForest(as.factor(diabetes) ~ .,   data=dftrain)
+fit <- randomForest(as.factor(hcluster) ~ .,   data=dftrain)
 print(fit) # view results 
 importance(fit) # importance of each predictor
 varImpPlot(fit)
